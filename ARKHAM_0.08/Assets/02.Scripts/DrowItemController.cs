7a08f7f082a3f2576746690570af953d8abbc7b3
moonyh0918 <42628856+moonyh0918@users.noreply.github.com>
2018-09-03 01:25:57 +0900

    Item1 오브젝트에 딕셔너리 랜덤한 값 하나를 가져와서 출력

--- ARKHAM_0.08/Assets/02.Scripts/DrowItemController.cs
+++ ARKHAM_0.08/Assets/02.Scripts/DrowItemController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DrowItemController : MonoBehaviour {
 
 
     public GameObject DrowItemPanel;
+    public Image ItemImage;
 
 
 
@@ -20,8 +23,21 @@ public class DrowItemController : MonoBehaviour {
 
     public void DrowItemEncounter()
     {
+        
+        
         DrowItemPanel.SetActive(true);
 
+        if (ItemList.instance.NomalItemList.ContainsKey(5))
+        {
+            ItemList.Item item = ItemList.instance.NomalItemList[Random.Range(1,4)];
+
+            ItemImage.sprite = item.GetImageAction();
+
+            Debug.Log(item);
+            Debug.Log("help");
+        }
+        
+        
 
     }
 }

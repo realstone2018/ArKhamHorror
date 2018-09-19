ea1fc71fc29efbda5f5dd398e02f2b30939322cc
moonyh0918 <42628856+moonyh0918@users.noreply.github.com>
2018-09-20 05:14:45 +0900

    보스,캐릭터 선택 틀

--- ARKHAM_0.10/Assets/02.Scripts/GameStart.cs
+++ ARKHAM_0.10/Assets/02.Scripts/GameStart.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour {
 
+    public GameObject Startmenu;
+    public GameObject SelectBoss;
+    public GameObject SelectCharacter;
+
+    public void SelectbossBtn()
+    {
+        Startmenu.SetActive(false);
+        SelectBoss.SetActive(true);
+    }
+
+    public void SelectCharacterBtn()
+    {
+        SelectBoss.SetActive(false);
+        SelectCharacter.SetActive(true);
+
+    }
+
 	
     public void OnClickStartBtn()
     {
+        //캐릭터 초기 설정
         SceneManager.LoadScene("MainScene",LoadSceneMode.Single);
     }
 }
